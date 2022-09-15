8645fef40977a3fd49dfc660a60c890bf6ec7182
Papp David <pappd377@gmail.com>
2022-09-15 13:11:01 +0200

    commit

---
+++ src/lista.js
@@ -0,0 +1,14 @@
+let x=1;
+let y=0;
+let lista=[x];
+let rnd=Math.random(-10,10)
+while(rnd!=0){
+    if(rnd>0){
+        lista[y] +=rnd;
+        y++;
+        x++;
+    }
+    else{
+        alert();
+    }
+}
